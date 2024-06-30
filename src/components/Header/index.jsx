fd3d5af31ae03968562213ea379db0b1b29cb75d
felipe <enggazolla@gmail.com>
2024-06-30 19:16:48 -0300

    user image

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
-import { Container } from "./styles"
+import { Container, Profile } from "./styles"
 
 export function Header() {
   return (
     <Container>
-      
+      <Profile>
+        <img 
+          src="https://github.com/felipegazolla.png" 
+          alt="Foto do Felipe"
+        />
+
+        <div>
+          <span>Bem vindo</span>
+          <strong>Felipe Gazolla</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
