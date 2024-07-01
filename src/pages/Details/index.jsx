ef40e84f44c594850527a53bae60e16028846587
felipe <enggazolla@gmail.com>
2024-07-01 13:13:55 -0300

    tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   
@@ -16,6 +17,11 @@ export function Details() {
             <a href="#">Link 1</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="node"/>
       </Section>
 
       <Button title="Voltar" />
