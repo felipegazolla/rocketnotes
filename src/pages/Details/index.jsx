f6e565e63e87842e1ddbabb33d05efe84518613b
felipe <enggazolla@gmail.com>
2024-07-01 21:51:00 -0300

    finishing details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
 import { Tag } from '../../components/Tag'
 import { Button } from '../../components/Button'
@@ -7,27 +7,37 @@ import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
-  
+
   return (
     <Container>
       <Header />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <ButtonText title="Excluir nota" />
+            <h1>
+              Introdução ao React
+            </h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas quos reprehenderit magnam explicabo at consectetur sunt sint totam quasi officiis nam aspernatur hic, laboriosam est asperiores doloribus molestias expedita eos.
+            </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">Link 1</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">Link 1</a>
-          </li>
-        </Links>
-      </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node" />
+          </Section>
 
-      <Section title="Marcadores">
-        <Tag title="express"/>
-        <Tag title="node"/>
-      </Section>
+          <Button title="Voltar" />
 
-      <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
