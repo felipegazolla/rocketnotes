8a5dd8254d567f72f94f8b3ea6afae6b103c6e12
felipe <enggazolla@gmail.com>
2024-07-02 17:15:00 -0300

    filter

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -18,10 +18,33 @@ export const Container = styled.div`
 
 export const Brand = styled.div`
   grid-area: brand;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  border-bottom-color: ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
 `
 
 export const Menu = styled.ul`
   grid-area: menu;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+  
+  padding-top: 64px;
+  text-align: center;
+
+  > li {
+    margin-bottom: 24px;
+  }
 `
 
 export const Search = styled.div`
