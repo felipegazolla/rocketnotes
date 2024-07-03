74d665eb7dc15f1f12d3bf3333cc084906d355b1
felipe <enggazolla@gmail.com>
2024-07-03 16:53:07 -0300

    signin form

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,28 @@
-import { Container } from './styles'
+import { Container, Form } from './styles'
+import { FiMail, FiLock } from 'react-icons/fi'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
 
 export function SignIn() {
   return (
     <Container>
-      
+      <Form>
+        <h1>Rocketnotes</h1>
+        <p>Aplicação para gerenciar e salvar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input placeholder="E-mail" type="text" icon={FiMail} />
+        
+        <Input placeholder="Senha" type="password" icon={FiLock} />
+        
+        <Button title="Entrar"/>
+
+        <a href="#">
+          Criar conta
+        </a>
+      </Form>
     </Container>
   )
 }
