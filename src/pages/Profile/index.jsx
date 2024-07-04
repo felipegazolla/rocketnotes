a84f903d3596238f433899cf09c49ece04d1577f
felipe <enggazolla@gmail.com>
2024-07-03 23:48:46 -0300

    user avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi"
-import { Container, Form } from "./styles"
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi"
+import { Container, Form, Avatar } from "./styles"
 import { Input } from "../../components/Input"
 import { Button } from "../../components/Button"
 
@@ -13,6 +13,14 @@ export function Profile() {
       </header>
 
       <Form>
+        <Avatar>
+          <img src="https://github.com/felipegazolla.png" alt="Foto do usuário" />
+          
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input type="file" id="avatar" />
+          </label>
+        </Avatar>
         <Input placeholder="Nome" type="text" icon={FiUser} />
         <Input placeholder="E-mail" type="text" icon={FiMail} />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
